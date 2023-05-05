e0641b4c22a5c6d61878e29ebfa0378857f0543a
Priyanshuahuja72 <priyanshuahuja72@gmail.com>
2023-05-05 21:55:36 +0530

    sssssssssss

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.getElementById('ip').innerHTML = userIP;
 function GetUserIP(){
     var ret_ip;
     $.ajaxSetup({async: false});
-    $.get('http://jsonip.com/', function(r){ 
+    $.get('https://api.ipify.org?format=json', function(r){ 
         ret_ip = r.ip; 
     });
     return ret_ip;
@@ -68,52 +68,9 @@ async function postalData(pincode)
                 </div>`
     })
 }
-// function fetchData() {
-//     return new Promise((resolve) => {
-//     resolve(
-//     fetch(`https://ipinfo.io/${userIP}/json?token=${accessToken}`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//         console.log(data);
-//         lat.innerHTML = `Lat : ${data.loc.split(',')[0]}`;
-//         long.innerHTML = `Long : ${data.loc.split(',')[1]}`;
-//         city.innerHTML = `City : ${data.city}`;
-//         organisation.innerHTML = `Organisation : ${data.org}`;
-//         region.innerHTML = `Region : ${data.region}`;
-//         map.innerHTML = `<iframe src="https://maps.google.com/maps?q=${data.loc.split(',')[0]}, ${data.loc.split(',')[1]}&z=15&output=embed" width="360" height="270" frameborder="0" style="border:0"></iframe>`;
 
-//         timeZone.innerHTML = data.timezone;
-//         const today = new Date();
-//         dateTime.innerHTML = today.toISOString().split('T')[0] + " & " + today.toISOString().split('T')[1];
-//         pincode.innerHTML = data.postal;
-        
-//         return data.postal;
-//     })
-//     .then((pincode) => {
-//         fetch(`https://api.postalpincode.in/pincode/${pincode}`)
-//             .then((postalRes) => postalRes.json())
-//             .then((postalData) => {
-//                 console.log(postalData);
-                // message.innerHTML = postalData[0].Message;
-                // const postOffices = postalData[0].PostOffice;
-                // console.log(postOffices);
-                // postOffices.forEach((post) => {
-                //     postOfficesList.innerHTML += `<div class="post-office">
-                //     <div class="post-office-name">Name : ${post.Name}</div>
-                //     <div class="branch-type">Branch Type : ${post.BranchType}</div>
-                //     <div class="delivery-status">Delivery Status : ${post.DeliveryStatus}</div>
-                //     <div class="district">District : ${post.District}</div>
-                //     <div class="division">Division : ${post.Division}</div>
-                // </div>`
-                // })
-//             })
-//             .catch((error) => console.log(error));
-//     }))
-//     .catch((error) => console.log(error));
-// })
-// }
 
-// fetchData();
+fetchData();
 
 function updatePostOffices() {
     
